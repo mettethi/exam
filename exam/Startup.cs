944a3a30ec2ed75ec7b739b0f3b68479030b4376
Mette Thisgaard <mettethisgaard@hotmail.com>
2016-05-24 11:17:09 +0200

    first commit

---
+++ exam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(exam.Startup))]
+namespace exam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
