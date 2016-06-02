743d90434d6a76e4af2be7e906d432c6b7018868
Mette Thisgaard <mettethisgaard@hotmail.com>
2016-06-02 09:44:29 +0200

    Hub

--- exam/Models/HelplistHub.cs
+++ exam/Models/HelplistHub.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using exam.DAL;
+using exam.Controllers;
 
 namespace exam.Models
 {
     public class HelplistHub : Hub
     {
+        private ApplicationContext db = new ApplicationContext();
+
         public void Hello()
         {
             Clients.All.hello();
         }
 
-        public void AddToHelplist(int userId)
+
+        public void AddToHelplist(int userId, int assignmentId)
         {
             //gå i db og find user
-            User user = new User { ID = userId, Name = "MetteLisa" };
-            Clients.All.getHelplist(user);
+            // User user = new User { ID = userId, Name = "MetteLisa" };
+
+            User user = db.Users.Find(userId);
+            Assignment assignment = db.Assignments.Find(assignmentId);
+            HelplistsController controller = new HelplistsController();
+
+            Helplist help = new Helplist();
+            help.User = user;
+            help.Assignment = assignment;
+
+            controller.AddToHelplist(help);
+
+            Clients.All.getHelplist(user, assignment);
         }
+
+
     }
 }
