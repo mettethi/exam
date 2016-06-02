f6e6d008d68295d484fd195d239ed2d728740758
Lisa Daniella Olesen <lisa3097@stud.kea.dk>
2016-06-02 17:09:50 +0200

    Deleted unused stuff

--- exam/Startup.cs
+++ exam/Startup.cs
@@ -8,7 +8,7 @@ namespace exam
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+           //ConfigureAuth(app);
             app.MapSignalR();
         }
     }
